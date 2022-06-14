ec22a18096b151a205c8b2a87c8457ab8e9a0eff
son3102k <prokieumoi1@gmail.com>
2022-06-14 17:10:59 +0700

    resolve conflix

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/home';
 import StudentAccount from './Accounts/Students/StudentsAccount';
-import LecturertAccount from './Accounts/Lecturers/LecturerAccount';
+import LecturerAccount from './Accounts/Lecturers/LecturerAccount';
 import Subject from './Subject/SubjectList'
 import Timetable from './pages/student/Timetable';
 import Result from './pages/student/Result';
@@ -22,7 +22,7 @@ function App() {
                 <Route path="/admin/login" element={<Login role="admin" />}></Route>
                 <Route path="/lecturer/login" element={<Login role="lecturer" />}></Route>
                 <Route path="/accounts/students" element={<StudentAccount />}></Route>
-                <Route path="/accounts/lecturers" element={<LecturertAccount />}></Route>
+                <Route path="/accounts/lecturers" element={<LecturerAccount />}></Route>
                 <Route path="/subjects" element={<Subject />}></Route>
                 <Route path="/student/timetable" element={<Home role="student"><Timetable></Timetable></Home>}></Route>
                 <Route path="/student/result" element={<Home role="student"><Result></Result></Home>}></Route>
